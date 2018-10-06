0a3b7f79333e97b726892e40603805a389dd8eef
Todd <mopeybear@gmail.com>
2018-10-05 20:40:48 -0400

    Fizzbuzz extended to FizzBuzzQix

--- function.js
+++ function.js
@@ -7,8 +7,21 @@ const functions = {
         value = '';
         if (num % 3 == 0) value += 'Fizz';
         if (num % 5 == 0) value += 'Buzz';
+        if (num % 7 == 0) value += 'Qix';
+
+       value += functions.fizzBuzzValue(num);
         if (value != '') return value;
         return num + '';
+    },
+
+    fizzBuzzValue: num => {
+        value = '';
+        for(var i = num; i > 0; i = Math.floor(i / 10)) {
+            if (i % 10 == 3) value = 'Fizz' + value; 
+            if (i % 10 == 5) value = 'Buzz' + value;
+            if (i % 10 == 7) value = 'Qix' + value;
+        }
+        return value;
     },
 
     fetchLuke: () => axios.get('https://swapi.co/api/people/1')
--- function.test.js
+++ function.test.js
@@ -13,16 +13,32 @@ test('FizzBuzz of 2 is 2', function() {
     expect(functions.fizzBuzz(2)).toBe('2');
 });
 
-test('FizzBuzz of 3 is Fizz', function() {
-    expect(functions.fizzBuzz(3)).toBe('Fizz');
+test('FizzBuzz of 3 is FizzFizz', function() {
+    expect(functions.fizzBuzz(3)).toBe('FizzFizz');
 });
 
-test('FizzBuzz of 5 is Buzz', function() {
-    expect(functions.fizzBuzz(5)).toBe('Buzz');
+test('FizzBuzz of 5 is BuzzBuzz', function() {
+    expect(functions.fizzBuzz(5)).toBe('BuzzBuzz');
 });
 
-test('FizzBuzz of 15 is FizzBuzz', function() {
-    expect(functions.fizzBuzz(15)).toBe('FizzBuzz');
+test('FizzBuzz of 15 is FizzBuzzBuzz', function() {
+    expect(functions.fizzBuzz(15)).toBe('FizzBuzzBuzz');
+});
+
+test('Fizzbuzz of 7 will return QixQix', function() {
+    expect(functions.fizzBuzz(7)).toBe('QixQix');
+});
+
+test('Fizzbuzz of 21 will return FizzQix', function() {
+    expect(functions.fizzBuzz(21)).toBe('FizzQix');
+});
+
+test('FizzBuzz of 35 will return BuzzQixFizzBuzz', function() {
+    expect(functions.fizzBuzz(35)).toBe('BuzzQixFizzBuzz');
+});
+
+test('FizzBuzz of 735 will return FizzBuzzQixQixFizzBuzz', function() {
+    expect(functions.fizzBuzz(735)).toBe('FizzBuzzQixQixFizzBuzz');
 });
 
 test('user fetched films totals 5', () => {
